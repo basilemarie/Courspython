e2aa5fc421d8cfa469bdd9a2e7ecde25375b585e
MARIE Basile <basile.marie67@gmail.com>
2024-03-14 15:14:27 +0100

    résultat avec le dictionnaire donné

--- TD1/Exo3.py
+++ TD1/Exo3.py
@@ -1,9 +1,17 @@
 #We can use a dictionnary to represent the points of each letters
+available_words = []
+file = open("frenchssaccent.dic",'r')
+for row in file:
+    available_words.append(row[0:len(row)-1])
+file.close()
+
+points = {'a' : 1, 'e' : 1,'i' : 1,'l' : 1,'n' : 1,'o' : 1,'r' : 1,'s' : 1,'t' : 1,'u' : 1,'d' : 2,'g' : 2,'m' : 2,'b' : 3,'c' : 3,'p' : 3,'f' : 4,'h' : 4,'v' : 4,'j' : 8,'q' : 8,'k' : 10,'w' : 10,'x' : 10,'y' : 10,'z' : 10,'-':0}
+
 
 def score(words,point):
     score = 0
     for letter in words : 
-        score += point[letter]
+        score = score + point[letter]
     return(score)
 
 assert(score('ab',{'a':1,'b':2})==3) #Rapid verification
@@ -24,25 +32,21 @@ assert(max_score(['rte', 'ver', 'ce'],{'r':2,'v':6,'e':1,'c':4,'t':4})==('ver',9
 #print(r)
 
 def high_score(tirage,words,point): 
-    value = len(words)
-    for interation in range(value):#be certain to cover all the words
-        letters = tirage.copy()
+    max = 0
+    for word in words:#be certain to cover all the words
+        letters = list(tirage) #making sure that tirage isn't changed
         useful = 1
-        (word, scoring) = max_score(words,point)
-        print(letters)
         for letter in word:
-            if letter not in letters:
+            if letter not in letters: #we can't use the word
                 useful = 0
             else:
-                letters.remove(letter)
+                letters.remove(letter) #making sure that one letter is not repeted in the word 
 
-        if useful==1: #no letters can be missed and the words are only constituted by letters of the lexic
-            bestword = word
-            bestscore = scoring
-            return((bestword,bestscore))
-        elif useful ==0:
-            words.remove(word)
+        if useful==1 and score(word,point)> max: #no letters can be missed and the words are only constituted by letters of the lexic 
+            max = score(word,point)
+            solution = word
+    return (solution,max)
     
 
-r = high_score(['r','t','e','c','v'],['rte', 'ver', 'ce','vatu','verre'],{'r':2,'v':6,'e':1,'c':4,'t':4,'a':10,'u':15})
+r = high_score(['a', 'r', 'b', 'g', 'e', 's', 'c', 'j'],available_words,points)
 print((r))
