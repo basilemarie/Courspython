ce5e1e99fb0550ffe9c77af43dc6cd31b2f02a70
MARIE Basile <basile.marie67@gmail.com>
2024-03-14 14:23:43 +0100

    modification de l'exercice 1 pour être sur de na pas compter deux fois la même lettre et résolution du derniers codes de l'exo 3

--- TD1/Exo1.py
+++ TD1/Exo1.py
@@ -1,15 +1,24 @@
 def lexical(tirage,words):
     max = 0  
+    useful = 1
     for word in words:
+        letters = tirage.copy()
         if len(word)>max: #not useful to seek for words smaller than the max 
             count = 0 #counter for the number of letters 
-            for letters in word:
-                if letters in tirage:
+            for letter in word:
+                if letter in letters:
                     count = count+1
-            if count>max and count == len(word): #no letters can be missed and the words are only constituted by letters of the lexic
+                    letters.remove(letter)
+                else:
+                    useful = 0
+            if count>max and useful ==1: #no letters can be missed and the words are only constituted by letters of the lexic
                 solution = word
-                max = count
+                max = count  
+                
     return(solution)
 
-assert(lexical(['b', 'p', 'd', 'w', 's', 'y', 'w', 'i'],['bis', 'bd'])=='bis')
+final = lexical(['b', 'p', 'd', 'w', 's', 'y', 'w', 'i'],['bis', 'bd','wsibpa','bisi'])
+print('Le mot le plus long pouvant être écris avec les lettres du tirage est', final)
+
+
 
--- TD1/Exo3.py
+++ TD1/Exo3.py
@@ -3,7 +3,7 @@
 def score(words,point):
     score = 0
     for letter in words : 
-        score = score + point[letter]
+        score += point[letter]
     return(score)
 
 assert(score('ab',{'a':1,'b':2})==3) #Rapid verification
@@ -20,16 +20,29 @@ def max_score(words,point):
 
 assert(max_score(['rte', 'ver', 'ce'],{'r':2,'v':6,'e':1,'c':4,'t':4})==('ver',9))
 
-#r = max_score(['rte', 'ver', 'ce'],{'r':2,'v':6,'e':1,'c':4,'t':4})
+#r = max_score(['rte', 'ver', 'ce','vatu'],{'r':2,'v':6,'e':1,'c':4,'t':4,'a':10,'u':15})
 #print(r)
 
-def high_score(tirage,words,point):
-    word,maxpoint = max_score(words,point)
-    for letter in word:
-        if letter not in tirage:
-            words.remove(word)
-            high_score(tirage,words,point)
-    return(word) #we're sure that the letter of the word are in tirage and the word is the highest in score
+def high_score(tirage,words,point): 
+    value = len(words)
+    for interation in range(value):#be certain to cover all the words
+        letters = tirage.copy()
+        useful = 1
+        (word, scoring) = max_score(words,point)
+        print(letters)
+        for letter in word:
+            if letter not in letters:
+                useful = 0
+            else:
+                letters.remove(letter)
 
-r = high_score(['r','t','e','c','v'],['rte', 'ver', 'ce','vatu'],{'r':2,'v':6,'e':1,'c':4,'t':4,'a':10,'u':15})
+        if useful==1: #no letters can be missed and the words are only constituted by letters of the lexic
+            bestword = word
+            bestscore = scoring
+            return((bestword,bestscore))
+        elif useful ==0:
+            words.remove(word)
+    
+
+r = high_score(['r','t','e','c','v'],['rte', 'ver', 'ce','vatu','verre'],{'r':2,'v':6,'e':1,'c':4,'t':4,'a':10,'u':15})
 print((r))
